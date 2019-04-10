f5f24bded17dc9bebe29dbf0d24aa5a193b1f69e
AnthonyWhitehead <antdwhitehead@gmail.com>
2019-04-10 21:00:43 +0100

    it lives

--- binary-tree.py
+++ binary-tree.py
@@ -5,33 +5,59 @@ import random
 class Tree:
     def __init__(self):
         self.root = None
-        self.nodes = []
 
-    def new_node(self, n):
-        if not self.root:
+    def add_node(self, val):
+        n = Node(val)
+        if self.root is None:
             self.root = n
         else:
-            self.nodes.append(n)
+            self.root.add_node(n)
 
 
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, val):
+        self.val = val
         self.right = None
         self.left = None
 
+    def add_node(self, n):
+        if n.val < self.val:
+            if self.left is None:
+                self.left = n
+            else:
+                self.left.add_node(n)
+        if n.val > self.val:
+            if self.right is None:
+                self.right = n
+            else:
+                self.right.add_node(n)
+
 
 def main():
+    data = [5, 92, 1, 3, 34, 4, 6, 38]
     tree = Tree()
-    for x in range(5):
+    for d in data:
+        tree.add_node(d)
 
-        # n = Node(5)
-        # tree.new_node(n)
-        tree.new_node(Node(random.randint(1, 101)))
+    return tree.root
 
-    t = vars(tree)
-    print(t)
+
+def search(val, node):
+    if node is None or node.val == val:
+        return node
+    if val < node.val:
+        return search(val, node.left)
+    if val > node.val:
+        return search(val, node.right)
+
+
+def find(key):
+    node = search(key, main())
+    dict = {
+        "val": node.val
+    }
+    print(dict)
 
 
 if __name__ == "__main__":
-    main()
+    find(34)
